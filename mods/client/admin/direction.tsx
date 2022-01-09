a52bab0d012e7cde6f3540450d0c042c02eb6943
daniel <d.asher.gottlieb@gmail.com>
2022-01-08 22:02:52 -0500

    adding room direction filters

---
+++ mods/client/admin/direction.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { Room } from './room'
+
+const DIRECTIONS = ['north', 'south', 'east', 'west', 'up', 'down', 'in', 'out']
+
+interface Props {
+    room: Room
+    onAdd: (input) => void
+}
+
+export const DirectionComponent: React.FunctionComponent<Props> = (props) => {
+    const {room} = props
+    const [direction, setDirection] = React.useState('')
+    const [name, setName] = React.useState('')
+
+    const add = () => {
+        props.onAdd({
+            direction,
+            room: name
+        })
+        setName('')
+        setDirection('')
+    }
+
+    return <Form.Group className="mb-3">
+        <Form.Label>Connections</Form.Label>
+        <div style={{display:'flex'}}>
+            <div style={{marginRight:8, width: 150}}>
+                <Form.Label>Direction</Form.Label>
+                <Form.Select value={direction} onChange={e => setDirection(e.target.value)}>
+                    {DIRECTIONS.filter(dir => {
+                        if (!room) return
+                        return !room.connections.find(x => x.direction === dir)
+                    })
+                    .map(dir => <option key={dir} value={dir}>{dir}</option>)}
+                </Form.Select>
+            </div>
+            <div style={{marginRight:8, width: '100%'}}>
+                <Form.Label>Room</Form.Label>
+                <Form.Select value={name} onChange={e => setName(e.target.value)}>
+                    <option value='1'>name1</option>
+                    <option value='2'>name2</option>
+                    <option value='3'>name3</option>
+                </Form.Select>
+            </div>
+            <Button style={{marginTop: 32}} onClick={() => add()}>
+                Add
+            </Button>
+        </div>
+    </Form.Group>
+}
--- mods/client/admin/room.tsx
+++ mods/client/admin/room.tsx
@@ -7,12 +7,20 @@ import { FaEdit, FaTrashAlt } from 'react-icons/fa'
 import Offcanvas from 'react-bootstrap/Offcanvas'
 
 import { ConfirmComponent } from '../components/confirm'
+import { DirectionComponent } from './direction'
 
+const DIRECTIONS = ['north', 'south', 'east', 'west', 'up', 'down', 'in', 'out']
+
+
+export class Room {
+    connections:{direction: string, room: string}[] = []
+}
+const useZones = () => ['asdf1', 'asdf2', 'asdf3']
 
 export const RoomFormComponent:React.FunctionComponent = () => {
-    const [zone, setZone] = React.useState('')
-    const [area, setArea] = React.useState(undefined)
-
+    const zones = useZones()
+    const [zone, setZone] = React.useState(zones[0])
+    const [room, setRoom]: [Room, (a: Room|undefined)=>void] = React.useState(undefined as any)
 
     const renderListItem = (item):JSX.Element => {
         return <ListGroupItem key={item}
@@ -22,27 +30,38 @@ export const RoomFormComponent:React.FunctionComponent = () => {
             alignItems: 'center'}}>
             {item}
             <div>
-                <Button style={{marginLeft: 8}} variant="outline-warning" onClick={() => setArea({} as any)} >
+                <Button style={{marginLeft: 8}} variant="outline-warning" onClick={() => setRoom(new Room())} >
                     <FaEdit color='warning' />
                 </Button>
                 <ConfirmComponent header={`Delete ${item}`} message='Are you sure you want to delete?'>
-                <Button style={{marginLeft:8}} variant="outline-danger">
-                    <FaTrashAlt color="danger" />
-                </Button>
+                    <Button style={{marginLeft:8}} variant="outline-danger">
+                        <FaTrashAlt color="danger" />
+                    </Button>
                 </ConfirmComponent>
             </div>
         </ListGroupItem>
     }
 
-    const renderConnectionItem = ():JSX.Element => {
+    const renderConnectionItem = (item):JSX.Element => {
         return <div style={{display:'flex'}}>
-            <Form.Select style={{marginRight:8}}></Form.Select>
-            <Form.Select style={{marginRight:8}}></Form.Select>
+            <Form.Select value={item.direction} style={{marginRight:8}} onChange={() => console.log('')}>
+                {DIRECTIONS.filter(dir => {
+                    if (dir === item.direction) return true
+                    return !room.connections.find(x => x.direction === dir)
+                }).map(dir => <option key={dir} value={dir}>{dir}</option>)}
+            </Form.Select>
+            <Form.Select style={{marginRight:8}} onChange={() => console.log('')}></Form.Select>
             <Button variant='outline-danger'><FaTrashAlt /></Button>
         </div>
     }
 
+    const addDirection = (dir) => {
+        room.connections.push(dir)
+        setRoom({...room})
+    }
+
     const renderForm = ():JSX.Element => {
+        if (!room) return <></>
         return <Form>
             <Form.Group className="mb-3">
                 <Form.Label>Name</Form.Label>
@@ -52,30 +71,23 @@ export const RoomFormComponent:React.FunctionComponent = () => {
                 <Form.Label>Description</Form.Label>
                 <textarea className='form-control'></textarea>
             </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Connections</Form.Label>
-                <div style={{display:'flex'}}>
-                    <Form.Select style={{marginRight:8}}></Form.Select>
-                    <Form.Select style={{marginRight:8}}></Form.Select>
-                    <Button>Add</Button>
-                </div>
-            </Form.Group>
+            <DirectionComponent onAdd={addDirection} room={room} />
             <hr />
             <Form.Group className="mb-3">
                 <ListGroup>
-                    {renderConnectionItem()}
+                    {room.connections.map(conn => renderConnectionItem(conn))}
                 </ListGroup>
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Tags</Form.Label>
             </Form.Group>
             <Button variant='outline-primary'>Save</Button>
-            <Button variant='outline-secondary' onClick={() => setArea(undefined)}>Cancel</Button>
+            <Button variant='outline-secondary' onClick={() => setRoom(undefined)}>Cancel</Button>
         </Form>
     }
 
     return <>
-        <Offcanvas style={{width: 600}} show={area}>
+        <Offcanvas style={{width: 600}} show={room}>
             <Offcanvas.Header>
                 <Offcanvas.Title>Area Form</Offcanvas.Title>
             </Offcanvas.Header>
@@ -88,15 +100,12 @@ export const RoomFormComponent:React.FunctionComponent = () => {
             <Form.Group className="mb-3">
                 <Form.Label>Zone</Form.Label>
                 <Form.Select onChange={e => setZone(e.target.value)}>
-                    <option value={1}>zone1</option>
-                    <option value={2}>zone2</option>
-                    <option value={3}>zone3</option>
-                    <option value={4}>zone4</option>
+                    {zones.map(z => <option key={z} value={z}>{z}</option>)}
                 </Form.Select>
             </Form.Group>
 
             <Form.Group className="mb-3" style={{display: 'flex', justifyContent: 'center'}}>
-                <Button disabled={zone === ''} onClick={() => setArea({} as any)}>Add Room</Button>
+                <Button disabled={zone === ''} onClick={() => setRoom(new Room())}>Add Room</Button>
             </Form.Group>
         </Form>
 
